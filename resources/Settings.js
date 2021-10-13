05b8b00072ed15801f89446fb2504b17659bab66
vhada7 <hadav@rpi.edu>
2021-10-13 05:09:53 -0400

    Basic Settings Page

---
+++ resources/Settings.js
@@ -0,0 +1,10 @@
+//JavaScript Form for Settings
+
+function Settings() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
