22c673d949a3a8d91cbfec7ede99feeb7733b884
RussellABH <75578919+RussellABH@users.noreply.github.com>
2021-10-14 23:19:34 -0400

    Prep for merging into main
    A bunch of changes to make this look like main before merge

---
+++ resources/main.js
@@ -0,0 +1,26 @@
+function changeTheme(themeNum) {
+    $.getJSON("resources/themes.json", data => {
+        document.getElementById("homepageBody").style.backgroundColor = data["themes"][themeNum]["backgroundColor"];
+        document.getElementById("homepageBody").style.backgroundImage = data["themes"][themeNum]["backgroundImage"];
+
+        document.getElementById("bottomBar").style.backgroundColor = data["themes"][themeNum]["barColor"];
+        document.getElementById("bottomBar").style.backgroundImage = data["themes"][themeNum]["barImage"];
+        });
+}
+
+//For Settings
+
+function Settings() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+//Make default theme there
+if (window.location.pathname == "/index.html") {
+    changeTheme(0);
+}
