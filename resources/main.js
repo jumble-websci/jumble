3a8526e97fcc5c491f98c0054dc4ff2f03b97dc3
nothingexessive <liuxjames88@gmail.com>
2021-11-27 11:50:54 -0500

    Updating code
    Changed the drag and drop from vanilla js to jquery

--- resources/main.js
+++ resources/main.js
@@ -26,48 +26,53 @@ if (window.location.pathname == "/index.html") {
 }
 
 
-
-document.addEventListener('DOMContentLoaded', (event) => {
-
-  var dragSrcEl = null;
-  
-  function handleDragStart(e) {
-    
-    dragSrcEl = this;
-
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-  }
-
-  function handleDragOver(e) {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
-
-    e.dataTransfer.dropEffect = 'move';
-    
-    return false;
-  }
+$( function() {
+    $( ".container" ).sortable();
+} );
 
 
-  function handleDrop(e) {
-    if (e.stopPropagation) {
-      e.stopPropagation(); // stops the browser from redirecting.
-    }
-    
-    if (dragSrcEl != this) {
-      dragSrcEl.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData('text/html');
-    }
-    
-    return false;
-  }
-  
-  
-  let items = document.querySelectorAll('.container .box');
-  items.forEach(function(item) {
-    item.addEventListener('dragstart', handleDragStart, false);
-    item.addEventListener('dragover', handleDragOver, false);
-    item.addEventListener('drop', handleDrop, false);
-  });
-});
+//
+// document.addEventListener('DOMContentLoaded', (event) => {
+//
+//   var dragSrcEl = null;
+//
+//   function handleDragStart(e) {
+//
+//     dragSrcEl = this;
+//
+//     e.dataTransfer.effectAllowed = 'move';
+//     e.dataTransfer.setData('text/html', this.innerHTML);
+//   }
+//
+//   function handleDragOver(e) {
+//     if (e.preventDefault) {
+//       e.preventDefault();
+//     }
+//
+//     e.dataTransfer.dropEffect = 'move';
+//
+//     return false;
+//   }
+//
+//
+//   function handleDrop(e) {
+//     if (e.stopPropagation) {
+//       e.stopPropagation(); // stops the browser from redirecting.
+//     }
+//
+//     if (dragSrcEl != this) {
+//       dragSrcEl.innerHTML = this.innerHTML;
+//       this.innerHTML = e.dataTransfer.getData('text/html');
+//     }
+//
+//     return false;
+//   }
+//
+//
+//   let items = document.querySelectorAll('.container .box');
+//   items.forEach(function(item) {
+//     item.addEventListener('dragstart', handleDragStart, false);
+//     item.addEventListener('dragover', handleDragOver, false);
+//     item.addEventListener('drop', handleDrop, false);
+//   });
+// });
