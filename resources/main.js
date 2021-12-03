9be9af617c08e04d0b9b0ac0d6888b65ed84cf8d
James Liu <liuxjames88@gmail.com>
2021-12-03 04:40:45 -0500

    groups will no longer expand when being dragged

--- resources/main.js
+++ resources/main.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
     if (window.location.pathname == "/index.html") {
         changeTheme(0);
     }
-    $( ".container" ).sortable();
-    // alert("done");
+    $( ".container" ).sortable({
+      start: function(event, ui){
+        ui.item.addClass('noclick');
+        },
+      update: function( event, ui ) {
+        test(event, ui)
+      }
+    });
 });
 
+let test1;
+let test2;
+
+// TODO: make groups follow where the item has been dragged
+function test (event, ui) {
+  test1 = event;
+  test2 = ui;
+
+}
 
 function changeTheme(themeNum) {
     $.getJSON("resources/themes.json", data => {
@@ -40,71 +55,21 @@ function show(id) {
         $("#"+id+"+.newline").remove();
         $("#"+id).remove();
     }
-    
-    // $("<span></span>").insertAfter("#"+id);
-    // $( "<p class='group'>Test</p>" ).insertAfter( "#group"+id );
 }
 
 
-function group(id) {
-    // $("#"+id).toggle('fast');
-    // if (!$("#"+id).length) {
-    //     $("<span class='newline'></span>").insertAfter("#"+id);
-    // } else {
-    //     $("#"+id+"+.newline").remove();
-    // }
-    
-    $("."+id).toggle('fast');
-}
+$(".group-class").click(function(){
+  if ($(this).hasClass('noclick')) {
+    $(this).removeClass('noclick');
+  }
+  else {
+    $(this).next().slideToggle();
+    $(this).find('.ui-icon').toggleClass("ui-icon-minusthick").toggleClass("ui-icon-plusthick");
+  }
+});
+  
 
 function change() {
     $("#bottomBar").children().children().removeClass("box").addClass("box-small");
     $("#iconArea").children().children().removeClass("box-small").addClass("box");
 }
-
-
-//
-// document.addEventListener('DOMContentLoaded', (event) => {
-//
-//   var dragSrcEl = null;
-//
-//   function handleDragStart(e) {
-//
-//     dragSrcEl = this;
-//
-//     e.dataTransfer.effectAllowed = 'move';
-//     e.dataTransfer.setData('text/html', this.innerHTML);
-//   }
-//
-//   function handleDragOver(e) {
-//     if (e.preventDefault) {
-//       e.preventDefault();
-//     }
-//
-//     e.dataTransfer.dropEffect = 'move';
-//
-//     return false;
-//   }
-//
-//
-//   function handleDrop(e) {
-//     if (e.stopPropagation) {
-//       e.stopPropagation(); // stops the browser from redirecting.
-//     }
-//
-//     if (dragSrcEl != this) {
-//       dragSrcEl.innerHTML = this.innerHTML;
-//       this.innerHTML = e.dataTransfer.getData('text/html');
-//     }
-//
-//     return false;
-//   }
-//
-//
-//   let items = document.querySelectorAll('.container .box');
-//   items.forEach(function(item) {
-//     item.addEventListener('dragstart', handleDragStart, false);
-//     item.addEventListener('dragover', handleDragOver, false);
-//     item.addEventListener('drop', handleDrop, false);
-//   });
-// });
