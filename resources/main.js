2e0d88d1cad6f9da4e885553b1515eb7430e76d4
vhada7 <hadav@rpi.edu>
2021-12-05 14:18:17 -0500

    Settings

--- resources/main.js
+++ resources/main.js
@@ -5,7 +5,20 @@ function changeTheme(themeNum) {
 
         document.getElementById("bottomBar").style.backgroundColor = data["themes"][themeNum]["barColor"];
         document.getElementById("bottomBar").style.backgroundImage = data["themes"][themeNum]["barImage"];
+
+       
         });
+}
+
+
+function hover(i){
+  
+  $.getJSON("resources/themes.json", data => {
+    document.getElementsByClassName("theme")[i].style.backgroundImage = data["themes"][i]["backgroundImage"];
+
+   
+    });
+   
 }
 
 //For Settings
@@ -19,6 +32,14 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 }
 
+function hideform() {
+  document.getElementById("info").style.display = "none";
+}
+
+function showform() {
+  document.getElementById("info").style.display = "block";
+}
+
 
 //Make default theme there
 if (window.location.pathname == "/index.html") {
@@ -26,53 +47,48 @@ if (window.location.pathname == "/index.html") {
 }
 
 
-$( function() {
-    $( ".container" ).sortable();
-} );
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  var dragSrcEl = null;
+  
+  function handleDragStart(e) {
+    
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    e.dataTransfer.dropEffect = 'move';
+    
+    return false;
+  }
 
 
-//
-// document.addEventListener('DOMContentLoaded', (event) => {
-//
-//   var dragSrcEl = null;
-//
-//   function handleDragStart(e) {
-//
-//     dragSrcEl = this;
-//
-//     e.dataTransfer.effectAllowed = 'move';
-//     e.dataTransfer.setData('text/html', this.innerHTML);
-//   }
-//
-//   function handleDragOver(e) {
-//     if (e.preventDefault) {
-//       e.preventDefault();
-//     }
-//
-//     e.dataTransfer.dropEffect = 'move';
-//
-//     return false;
-//   }
-//
-//
-//   function handleDrop(e) {
-//     if (e.stopPropagation) {
-//       e.stopPropagation(); // stops the browser from redirecting.
-//     }
-//
-//     if (dragSrcEl != this) {
-//       dragSrcEl.innerHTML = this.innerHTML;
-//       this.innerHTML = e.dataTransfer.getData('text/html');
-//     }
-//
-//     return false;
-//   }
-//
-//
-//   let items = document.querySelectorAll('.container .box');
-//   items.forEach(function(item) {
-//     item.addEventListener('dragstart', handleDragStart, false);
-//     item.addEventListener('dragover', handleDragOver, false);
-//     item.addEventListener('drop', handleDrop, false);
-//   });
-// });
+  function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+    
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    
+    return false;
+  }
+  
+  
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('drop', handleDrop, false);
+  });
+});
