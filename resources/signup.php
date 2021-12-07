57623f4cfc05efcc3d949aaa812f7d95c6917aa7
james <liuxjames88@gmail.com>
2021-12-07 13:17:02 -0500

    Updating signup.php

--- resources/signup.php
+++ resources/signup.php
@@ -27,6 +27,15 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $clean_email = validate($_POST['email']);
     $clean_pass = validate($_POST['password']);
 
+    // Double check inputs aren't empty
+    if (empty($clean_email)) {
+        echo("Error: Please enter an email");
+        die();    
+    } else if (empty($clean_pass)) {
+        echo("Error: Please enter a password");
+        die();    
+    }
+
     // Check length of password
     if (strlen($clean_pass) < 10) {
         echo("Error: Password too short (min length of 10)");
@@ -45,17 +54,11 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         die();
     }
 
-    // Double check inputs aren't empty
-    if (empty($clean_email)) {
-        echo("Error: Please enter an email");
-        die();    
-    } else if (empty($clean_pass)) {
-        echo("Error: Please enter a password");
-        die();    
-    }
+
 
     // Check to see if the email already exists
     $count = count( ($db->query("SELECT * FROM users WHERE email='$clean_email'"))->fetchAll() ) != 0;
+
 
     if ($count != 0) { 
         echo("Error: That email is already in use. ");
@@ -66,5 +69,5 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $encoded_password = password_hash($clean_pass, PASSWORD_DEFAULT);
     $sql = "INSERT INTO `users` (`id`, `email`, `password`, `fname`, `lname`) VALUES (NULL, '$clean_email', '$encoded_password', NULL, NULL)";
     $db->query($sql);
-    die();
+    // die();
 }
