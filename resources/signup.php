bd2595886e4699c57479b162c5df77096a46a6e1
RussellABH <75578919+RussellABH@users.noreply.github.com>
2021-12-07 13:14:27 -0500

    hopefully fix issue

--- resources/signup.php
+++ resources/signup.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); 
+session_start();
 
 $dbhost = "localhost";
 $dbusername = "root";
@@ -8,57 +8,58 @@ $dbpassword = "";
 $dbname = "jumble";
 
 try {
-    $db = new PDO("mysql:host=$dbhost;dbname=$dbname","$dbusername","$dbpassword");
-} 
-catch (Exception $e) {
-    echo("Error: Failed to connect to database -> $e");
+    $db = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbusername", "$dbpassword");
+} catch (Exception $e) {
+    echo ("Error: Failed to connect to database -> $e");
     die();
 }
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    function validate($data) {
-       $data = trim($data);
-       $data = stripslashes($data);
-       $data = htmlspecialchars($data);
-       return $data;
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
     // Clean up the input
     $clean_email = validate($_POST['email']);
     $clean_pass = validate($_POST['password']);
 
+    // Double check inputs aren't empty
+    if (empty($clean_email)) {
+        echo ("Error: Please enter an email");
+        die();
+    } else if (empty($clean_pass)) {
+        echo ("Error: Please enter a password");
+        die();
+    }
+
+
     // Check length of password
     if (strlen($clean_pass) < 10) {
-        echo("Error: Password too short (min length of 10)");
+        echo ("Error: Password too short (min length of 10)");
         die();
     }
 
     // Check length of email
     if (strlen($clean_email) > 255) {
-        echo("Error: Please use a shorter email");
+        echo ("Error: Please use a shorter email");
         die();
     }
 
     // Check that email is an email format
     if (!filter_var($clean_email, FILTER_VALIDATE_EMAIL)) {
-        echo("Error: Invalid Email Format");
+        echo ("Error: Invalid Email Format");
         die();
     }
 
-    // Double check inputs aren't empty
-    if (empty($clean_email)) {
-        echo("Error: Please enter an email");
-        die();    
-    } else if (empty($clean_pass)) {
-        echo("Error: Please enter a password");
-        die();    
-    }
-
     // Check to see if the email already exists
-    $count = count( ($db->query("SELECT * FROM users WHERE email='$clean_email'"))->fetchAll() ) != 0;
+    $count = count(($db->query("SELECT * FROM users WHERE email='$clean_email'"))->fetchAll()) != 0;
 
-    if ($count != 0) { 
-        echo("Error: That email is already in use. ");
+    if ($count != 0) {
+        echo ("Error: That email is already in use. ");
         die();
     }
 
